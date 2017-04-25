4ac9f8143a16f8d877db7f598a6a266ac025d2a8
neoautomata <neoautomata@users.noreply.github.com>
2017-04-24 20:49:28 -0400

    Update main.c

--- main.c
+++ main.c
@@ -14,194 +14,151 @@
    limitations under the License.
 */
 
-#include <stdio.h>
-
 #include "common/cs_dbg.h"
 #include "common/json_utils.h"
 #include "common/platform.h"
 #include "frozen/frozen.h"
+#include "fw/src/mgos_adc.h"
 #include "fw/src/mgos_app.h"
+#include "fw/src/mgos_console.h"
 #include "fw/src/mgos_gpio.h"
 #include "fw/src/mgos_mqtt.h"
+#include "fw/src/mgos_timers.h"
 #include "fw/src/mgos_rpc.h"
 #include "fw/src/mgos_wifi.h"
 
-static void* TURN_ON = "n";
-static void* TURN_OFF = "f";
-static void* GET_STATE = "s";
-static void* ENABLE = "e";
-static void* DISABLE = "d";
+#include "lib/DHT/dht.h"
+dht_data *dht1;
 
-static bool sonoff_led_disabled = false;
-static bool sonoff_button_disabled = false;
+static void* NO_MOTION = 0;
+static void* MOTION_DETECTED = 1;
 
-static void led_on(bool on) {
-	if (sonoff_led_disabled) {
-		// high is off.
-		mgos_gpio_write(13, true);
-		return;
-	}
-
-	if (on) {
-		// low is on.
-		mgos_gpio_write(13, false);
-	} else {
-		// high is off.
-		mgos_gpio_write(13, true);
-	}
-}
-
-static void sonoff_relay_handler(struct mg_rpc_request_info *ri, void *cb_arg,
-                                 struct mg_rpc_frame_info *fi, struct mg_str args) {
+static void dht_handler(struct mg_rpc_request_info *ri, void *cb_arg,
+                        struct mg_rpc_frame_info *fi, struct mg_str args) {
 	struct mbuf fb;
 	struct json_out out = JSON_OUT_MBUF(&fb);
-	mbuf_init(&fb, 128);
 	struct sys_config *cfg = get_cfg();
+	mbuf_init(&fb, 192);
 
-	int err = 0;
-	if (cb_arg == TURN_ON) {
-		led_on(true);
-
-		// turn the relay on by setting it high
-		mgos_gpio_write(12, true);
-	} else if (cb_arg == TURN_OFF) {
-		led_on(false);
-
-		// turn the relay on by setting it low
-		mgos_gpio_write(12, false);
+	if(dht1->err != NULL) {
+		json_printf(&out, "{device_id: %Q, device_type: sensors, msg: %Q}", cfg->device.id, "last dht read failed");
+		mg_rpc_send_errorf(ri, 500, "%.*s", fb.len, fb.buf);
 	} else {
-		err = 500;
-		json_printf(&out, "no or invalid action provided to callback");
-	}
-
-	if (err == 0) {
-		json_printf(&out, "{device_id: %Q, device_type: sonoff, relay_on: %d}", cfg->device.id, mgos_gpio_read(12) ? 1 : 0);
+		json_printf(&out, "{device_id: %Q, device_type: sensors, temp: %.2f, humidity: %.2f}", cfg->device.id, dht1->temp, dht1->humidity);
 		mg_rpc_send_responsef(ri, "%.*s", fb.len, fb.buf);
-		if (cb_arg != GET_STATE && strlen(cfg->sonoff.mqtt_topic) > 0)
-			mgos_mqtt_pub(cfg->sonoff.mqtt_topic, fb.buf, fb.len);
-	} else {
-		mg_rpc_send_errorf(ri, err, "%.*s", fb.len, fb.buf);
 	}
-	
+
 	ri = NULL;
 	mbuf_free(&fb);
+
 	(void) args;
 	(void) cb_arg;
 	(void) fi;
 }
 
-static void sonoff_button_handler(struct mg_rpc_request_info *ri, void *cb_arg,
-                                  struct mg_rpc_frame_info *fi, struct mg_str args) {
+static void dht_read() {
+	struct sys_config *cfg = get_cfg();
 	struct mbuf fb;
 	struct json_out out = JSON_OUT_MBUF(&fb);
-	mbuf_init(&fb, 128);
+	mbuf_init(&fb, 192);
+
+	dht_data *tmp = Read_DHT(cfg->dht1.pin, cfg->dht1.farenheit);
+	free(dht1);
+	dht1 = tmp;
+	if(dht1->err != NULL) {
+		CONSOLE_LOG(LL_INFO, ("Read dht - err: %s", dht1->err));
+		json_printf(&out, "{device_id: %Q, device_type: sensors, sensor: dht1, err: %s}", cfg->device.id, dht1->err);
+	} else {
+		CONSOLE_LOG(LL_INFO, ("Read dht - temp: %.2f; humidity: %.2f", dht1->temp, dht1->humidity));
+		json_printf(&out, "{device_id: %Q, device_type: sensors, sensor: dht1, temp: %.2f, humidity: %.2f}", cfg->device.id, dht1->temp, dht1->humidity);
+	}
+	if (strlen(cfg->dht1.mqtt_topic) > 0) {
+		mgos_mqtt_pub(cfg->dht1.mqtt_topic, fb.buf, fb.len);
+	}
+
+	mbuf_free(&fb);
+}
+
+static void light_handler(struct mg_rpc_request_info *ri, void *cb_arg,
+                          struct mg_rpc_frame_info *fi, struct mg_str args) {
+	struct mbuf fb;
+	struct json_out out = JSON_OUT_MBUF(&fb);
 	struct sys_config *cfg = get_cfg();
+	mbuf_init(&fb, 192);
 
-	int err = 0;
-	if (cb_arg == ENABLE) {
-		sonoff_button_disabled = false;
-	} else if (cb_arg == DISABLE) {
-		sonoff_button_disabled = true;
-	} else {
-		err = 500;
-		json_printf(&out, "no or invalid action provided to callback");
-	}
+	json_printf(&out, "{device_id: %Q, device_type: sensors, sensor: light1, level: %d}", cfg->device.id, mgos_adc_read(0));
+	mg_rpc_send_responsef(ri, "%.*s", fb.len, fb.buf);
 
-	if (err == 0) {
-		json_printf(&out, "{device_id: %Q, device_type: sonoff, button_disabled: %d}", cfg->device.id, sonoff_button_disabled ? 1 : 0);
-		mg_rpc_send_responsef(ri, "%.*s", fb.len, fb.buf);
-		if (cb_arg != GET_STATE && strlen(cfg->sonoff.mqtt_topic) > 0)
-			mgos_mqtt_pub(cfg->sonoff.mqtt_topic, fb.buf, fb.len);
-	} else {
-		mg_rpc_send_errorf(ri, err, "%.*s", fb.len, fb.buf);
-	}
-	
 	ri = NULL;
 	mbuf_free(&fb);
+
 	(void) args;
 	(void) cb_arg;
 	(void) fi;
 }
 
-static void sonoff_led_handler(struct mg_rpc_request_info *ri, void *cb_arg,
-                               struct mg_rpc_frame_info *fi, struct mg_str args) {
+static void light_read() {
+	struct sys_config *cfg = get_cfg();
 	struct mbuf fb;
 	struct json_out out = JSON_OUT_MBUF(&fb);
-	mbuf_init(&fb, 128);
-	struct sys_config *cfg = get_cfg();
+	mbuf_init(&fb, 192);
 
-	int err = 0;
-	if (cb_arg == ENABLE) {
-		sonoff_led_disabled = false;
-		led_on(mgos_gpio_read(12) ? 1 : 0);
-	} else if (cb_arg == DISABLE) {
-		sonoff_led_disabled = true;
-		led_on(false);
-	} else {
-		err = 500;
-		json_printf(&out, "no or invalid action provided to callback");
-	}
+	int lvl = mgos_adc_read(0);
+	CONSOLE_LOG(LL_INFO, ("Read light - lvl: %d", lvl));
+	json_printf(&out, "{device_id: %Q, device_type: sensors, sensor: light1, level: %d}", cfg->device.id, lvl);
+	if (strlen(cfg->light1.mqtt_topic) > 0)
+		mgos_mqtt_pub(cfg->light1.mqtt_topic, fb.buf, fb.len);
 
-	if (err == 0) {
-		json_printf(&out, "{device_id: %Q, device_type: sonoff, led_disabled: %d}", cfg->device.id, sonoff_led_disabled ? 1 : 0);
-		mg_rpc_send_responsef(ri, "%.*s", fb.len, fb.buf);
-		if (cb_arg != GET_STATE && strlen(cfg->sonoff.mqtt_topic) > 0)
-			mgos_mqtt_pub(cfg->sonoff.mqtt_topic, fb.buf, fb.len);
-	} else {
-		mg_rpc_send_errorf(ri, err, "%.*s", fb.len, fb.buf);
-	}
-	
-	ri = NULL;
 	mbuf_free(&fb);
-	(void) args;
-	(void) cb_arg;
-	(void) fi;
 }
 
-static void sonoff_toggle() {
-	// Skip if the button is disabled.
-	if (sonoff_button_disabled)
-		return;
-
-	// toggle the LED and relay
-	mgos_gpio_toggle(13);
-	mgos_gpio_toggle(12);
-
+static void motion_interrupt(int pin, void *arg) {
 	struct sys_config *cfg = get_cfg();
-	if (strlen(cfg->sonoff.mqtt_topic) > 0) {
-		struct mbuf fb;
-		struct json_out out = JSON_OUT_MBUF(&fb);
-		mbuf_init(&fb, 128);
-		json_printf(&out, "{device_id: %Q, device_type: sonoff, relay_on: %d}", cfg->device.id, mgos_gpio_read(12) ? 1 : 0);
-		mgos_mqtt_pub(cfg->sonoff.mqtt_topic, fb.buf, fb.len);
-	}
 
+	struct mbuf fb;
+	struct json_out out = JSON_OUT_MBUF(&fb);
+	mbuf_init(&fb, 192);
+	CONSOLE_LOG(LL_INFO, ("Motion interrupt - motion detected: %d", arg == MOTION_DETECTED ? 1 : 0));
+	json_printf(&out, "{device_id: %Q, device_type: sensors, sensor: motion1, motion_detected: %d}", cfg->device.id, arg == MOTION_DETECTED ? 1 : 0);
+	if (strlen(cfg->light1.mqtt_topic) > 0)
+		mgos_mqtt_pub(cfg->light1.mqtt_topic, fb.buf, fb.len);
+
+	mbuf_free(&fb);
+	(void) pin;
 }
 
 enum mgos_app_init_result mgos_app_init(void) {
-	// Set LED and Relay pins to outputs and off
-	mgos_gpio_set_mode(13, MGOS_GPIO_MODE_OUTPUT);
-	mgos_gpio_write(13, true);
-	mgos_gpio_set_mode(12, MGOS_GPIO_MODE_OUTPUT);
-	mgos_gpio_write(12, false);
-
-	// register the sonoff RPC handlers
+	struct sys_config *cfg = get_cfg();
 	struct mg_rpc *c = mgos_rpc_get_global();
-	mg_rpc_add_handler(c, "Sonoff.Relay.On", "{}", sonoff_relay_handler, TURN_ON);
-	mg_rpc_add_handler(c, "Sonoff.Relay.Off", "{}", sonoff_relay_handler, TURN_OFF);
-	mg_rpc_add_handler(c, "Sonoff.Relay.Status", "{}", sonoff_relay_handler, GET_STATE);
-	
-	mg_rpc_add_handler(c, "Sonoff.Button.Enable", "{}", sonoff_button_handler, ENABLE);
-	mg_rpc_add_handler(c, "Sonoff.Button.Disable", "{}", sonoff_button_handler, DISABLE);
-	mg_rpc_add_handler(c, "Sonoff.Button.Status", "{}", sonoff_button_handler, GET_STATE);
-	
-	mg_rpc_add_handler(c, "Sonoff.LED.Enable", "{}", sonoff_led_handler, ENABLE);
-	mg_rpc_add_handler(c, "Sonoff.LED.Disable", "{}", sonoff_led_handler, DISABLE);
-	mg_rpc_add_handler(c, "Sonoff.LED.Status", "{}", sonoff_led_handler, GET_STATE);
+ 
+	if (cfg->dht1.enable) {
+		CONSOLE_LOG(LL_INFO, ("DHT1 enabled: pin=%d, farenheit=%d, period=%dms, mqtt_topic=%s", cfg->dht1.pin, cfg->dht1.farenheit, cfg->dht1.period, cfg->dht1.mqtt_topic));
 
-	// register a button handler to toggle state
-	mgos_gpio_set_button_handler(0, MGOS_GPIO_PULL_UP, MGOS_GPIO_INT_EDGE_ANY, 500, sonoff_toggle, NULL);
+		mgos_set_timer(cfg->dht1.period, 1, dht_read, NULL);
 
+		// Register the read handler.
+		mg_rpc_add_handler(c, "DHT1.Read", "{}", dht_handler, NULL);
+	}
+
+	if (cfg->light1.enable) {
+		CONSOLE_LOG(LL_INFO, ("Light1 enabled: period=%dms, mqtt_topic=%s", cfg->light1.period, cfg->light1.mqtt_topic));
+
+		mgos_set_timer(cfg->light1.period, 1, light_read, NULL);
+
+		// Register the read handler.
+		mg_rpc_add_handler(c, "Light1.Read", "{}", light_handler, NULL);
+	}
+	
+	if (cfg->motion1.enable) {
+		CONSOLE_LOG(LL_INFO, ("Motion1 enabled: pin=%d, mqtt_topic=%s", cfg->motion1.pin, cfg->motion1.mqtt_topic));
+
+		// register handlers that detect rising (motion detected) and
+		// falling (still) edges.
+		mgos_gpio_set_int_handler(cfg->motion1.pin, MGOS_GPIO_INT_EDGE_POS, motion_interrupt, MOTION_DETECTED);
+		mgos_gpio_set_int_handler(cfg->motion1.pin, MGOS_GPIO_INT_EDGE_NEG, motion_interrupt, NO_MOTION);
+		mgos_gpio_enable_int(cfg->motion1.pin);
+	}
+
+	mgos_wifi_add_on_change_cb(on_wifi_event, 0);
 	return MGOS_APP_INIT_SUCCESS;
 }
-
